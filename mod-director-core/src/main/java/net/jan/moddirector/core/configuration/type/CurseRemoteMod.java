0c34bd6b6b0dc7b0830699b4d394af5955bd878d
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-03-05 09:30:09 +0100

    Migrate to new CurseForge API
    Optional TODO: Utilise FasterXML for this endeavour
    As I don't know about the syntax, GSON will have to do for now

--- mod-director-core/src/main/java/net/jan/moddirector/core/configuration/type/CurseRemoteMod.java
+++ mod-director-core/src/main/java/net/jan/moddirector/core/configuration/type/CurseRemoteMod.java
@@ -1,11 +1,24 @@
 package net.jan.moddirector.core.configuration.type;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-
 import net.jan.moddirector.core.ModDirector;
 import net.jan.moddirector.core.configuration.*;
 import net.jan.moddirector.core.exception.ModDirectorException;
@@ -13,15 +26,6 @@ import net.jan.moddirector.core.manage.ProgressCallback;
 import net.jan.moddirector.core.util.IOOperation;
 import net.jan.moddirector.core.util.WebClient;
 import net.jan.moddirector.core.util.WebGetResponse;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.Map;
 
 public class CurseRemoteMod extends ModDirectorRemoteMod {
     private final int addonId;
@@ -69,12 +73,16 @@ public class CurseRemoteMod extends ModDirectorRemoteMod {
     @Override
     public RemoteModInformation queryInformation() throws ModDirectorException {
         try {
-            URL apiUrl = new URL(
-                    String.format("https://addons-ecs.forgesvc.net/api/v2/addon/%d/file/%d", addonId, fileId));
-
-            information = ConfigurationController.OBJECT_MAPPER.readValue(apiUrl, CurseAddonFileInformation.class);
+            URL apiUrl = new URL(String.format("https://api.curse.tools/v1/cf/mods/%s/files/%s", addonId, fileId));
+            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42");
+            JsonObject jsonObject;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+                jsonObject = new JsonParser().parse(reader).getAsJsonObject().getAsJsonObject("data");
+            }
+            information = ConfigurationController.OBJECT_MAPPER.readValue(jsonObject.toString(), CurseAddonFileInformation.class);
         } catch(MalformedURLException e) {
-            throw new ModDirectorException("Failed to create ForgeSVC api url", e);
+            throw new ModDirectorException("Failed to create curse.tools api url", e);
         } catch(JsonParseException e) {
             throw new ModDirectorException("Failed to parse Json response from curse", e);
         } catch(JsonMappingException e) {
@@ -83,13 +91,13 @@ public class CurseRemoteMod extends ModDirectorRemoteMod {
             throw new ModDirectorException("Failed to open connection to curse", e);
         }
 
-        return new RemoteModInformation(information.name, information.fileName);
+        return new RemoteModInformation(information.displayName, information.fileName);
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     private static class CurseAddonFileInformation {
         @JsonProperty
-        private String name;
+        private String displayName;
 
         @JsonProperty
         private String fileName;
@@ -98,6 +106,6 @@ public class CurseRemoteMod extends ModDirectorRemoteMod {
         private URL downloadUrl;
 
         @JsonProperty
-        private String[] gameVersion;
+        private String[] gameVersions;
     }
 }
