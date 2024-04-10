394f5d6017844571121ddbaa6876bb0d43a7df93
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-04-10 16:18:10 +0200

    Update user agent

--- mod-director-core/src/main/java/net/jan/moddirector/core/util/WebClient.java
+++ mod-director-core/src/main/java/net/jan/moddirector/core/util/WebClient.java
@@ -7,7 +7,7 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class WebClient {
-    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36";
+    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36";
 
     public static WebGetResponse get(URL url) throws IOException  {
         URLConnection connection = url.openConnection();
